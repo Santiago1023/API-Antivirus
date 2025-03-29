cb1f62e291326a9517a1d9739ee59420d8debcb6
Verónica Sierra <vssombre@outlook.com>
2025-03-29 14:40:19 -0500

    se crea el endpoint para retornar por consulta de usuario todas las oportunidades guardadas y se crea Dto para esta consulta por este se integra con el Front y queda operativo

--- Api-Antivirus/Interface/IUserOpportunities.cs
+++ Api-Antivirus/Interface/IUserOpportunities.cs
@@ -7,6 +7,7 @@ namespace Api_Antivirus.Interface
         Task<IEnumerable<UserOpportunitiesResponseDto>> GetAllAsync();
         Task<UserOpportunitiesResponseDto> GetByIdAsync(int id);
         Task<object?> GetFavoriteIdAsync(int user_id, int opportunity_id);
+        Task <List<UserOpportunitiesDetailedConsultDTO>> GetUserOpportunitiesDetailedAsync (int userId); //consulta por user
         Task<UserOpportunitiesResponseDto> CreateAsync(UserOpportunitiesRequestDto dto);
         Task UpdateAsync(int id, UserOpportunitiesRequestDto dto);
         Task DeleteAsync(int id);
